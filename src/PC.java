6ab85efe498866f7d492aa0f43865b8cb8dda105
Tymur Foshch <antariuz@gmail.com>
2020-08-23 23:41:53 +0300

    Release commit

--- src/PC.java
+++ src/PC.java
@@ -1,7 +1,65 @@
 public class PC {
 
-    public static void main(String[] args) {
+    private String motherboardModel;
+    private String videocardModel;
+    private String ramModel;
+    private String ssdModel;
+    private String powerSupplyModel;
+    private String caseModel;
 
+    public static class Builder {
+        private PC newPC;
+
+        public Builder() {
+            newPC = new PC();
+        }
+
+        public Builder withMotherboardModel(String motherboardModel) {
+            newPC.motherboardModel = motherboardModel;
+            return this;
+        }
+
+        public Builder withVideocardModel(String videocardModel) {
+            newPC.videocardModel = videocardModel;
+            return this;
+        }
+
+        public Builder withRamModel(String ramModel) {
+            newPC.ramModel = ramModel;
+            return this;
+        }
+
+        public Builder withSSDModel(String ssdModel) {
+            newPC.ssdModel = ssdModel;
+            return this;
+        }
+
+        public Builder withPowerSupplyModel(String powerSupplyModel) {
+            newPC.powerSupplyModel = powerSupplyModel;
+            return this;
+        }
+
+        public Builder withCaseModel(String caseModel) {
+            newPC.caseModel = caseModel;
+            return this;
+        }
+
+        public PC build() {
+            return newPC;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PC pc = new PC.Builder()
+                .withMotherboardModel("RAMPAGE 8000")
+                .withVideocardModel("GEFORCE 5000 RTX")
+                .withRamModel("Hynix DDR6 4500 Ghz")
+                .withSSDModel("ZALUPKA 1 Tb Russian Edition")
+                .withCaseModel("From SHIT and STICKS")
+                .build();
+
+        System.out.println("Case " + pc.caseModel + " with " + pc.ssdModel);
     }
 
 }
