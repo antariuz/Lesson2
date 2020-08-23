dde7b90e5ac16599747c1e10d56d56808ada7050
Tymur Foshch <antariuz@gmail.com>
2020-08-23 22:52:35 +0300

    Release commit

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,6 +1,19 @@
-public class Vehicle{
+public final class Vehicle {
+
+    final private int wheels;
+
+    public Vehicle(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
 
     public static void main(String[] args) {
+
+        Vehicle car = new Vehicle(4);
+        System.out.println("Object car has " + car.wheels + " wheels");
 
     }
 
